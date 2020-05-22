8be31d6eaaeddc258b085ad234bc9055d833091c
Damian <damianmdst@gmail.com>
2020-05-21 22:49:07 -0300

    Build sidebar with handlers

---
+++ src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Sidebar = () => {
+  const [city, setCity] = useState();
+
+  const handleTextChange = (e) => setCity(e.target.value);
+
+  const handleKeyPress = (e) => {
+    if (e.which === 13 || e.keyCode === 13) {
+      fetchWeather();
+    }
+  };
+
+  const fetchWeather = async () => {
+    // fetch api
+  };
+
+  return (
+    <section className="m-2">
+      <div className="relative text-center text-gray-600">
+        <input
+          className="bg-white w-4/5 h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+          name="serch"
+          onChange={handleTextChange}
+          onKeyPress={handleKeyPress}
+          placeholder="Search"
+          type="search"
+          value={city}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
