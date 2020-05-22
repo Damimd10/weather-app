2a0dce7be7c46216a87016fe56745d517c41018e
Damian <damianmdst@gmail.com>
2020-05-21 21:16:58 -0300

    Create basic template with sidebar and main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <main>
+      <div className="flex p-3 h-screen">
+        <div className="w-1/4 bg-gray-500 h-full">Sidebar</div>
+        <div className="w-3/4 bg-gray-400 h-full">Content</div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
