7dacc200f743df27ac8ad79d26b4db1207950aea
Damian <damianmdst@gmail.com>
2020-08-02 12:59:29 -0300

    Change title and description, and add proper readme

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <main>
       <div className="flex flex-wrap h-screen border-2 rounded shadow-md">
-        <div className="w-full bg-white lg:h-full xl:h-full xl:w-1/4">
+        <div className="w-full flex items-center justify-center bg-white lg:h-full xl:h-full xl:w-1/4">
           <Sidebar />
         </div>
-        <div className="w-full bg-gray-100 h-full xl:w-3/4">
+        <div className="w-full bg-gray-100 flex items-center justify-center h-full xl:w-3/4">
           <Content />
         </div>
       </div>
